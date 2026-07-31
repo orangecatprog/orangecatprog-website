a4cdcf2fe88d0f7bbcdf22f0c20b2cbfc89f89ca
orangecatprog <luisrguezgam@gmail.com>
2026-07-31 23:33:56 +0200

    chore(git): create default scopes for commitizen

--- cz.config.ts
+++ cz.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from "cz-git";
 export default defineConfig({
     maxSubjectLength: 100,
     allowBreakingChanges: ["feat", "fix"],
+    scopes: ["git", "linting", "editor"],
 });
